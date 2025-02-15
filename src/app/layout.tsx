5bfd59c03b461051f75628b2109418631e695a2b
ahmedyoussef-b <139514412+ahmedyoussef-b@users.noreply.github.com>
2025-02-15 18:58:04 +0100

    Modification du texte de la page d'accueil3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lama Dev School Management Dashboard",
-  description: "Next.js School Management System",
+  title: "demarrage cycle combiné etape B",
+  description: "ahmed-abbes",
 };
 
 export default function RootLayout({
